fe086981ca13cac7256e8a96d24e61a891dab49d
scottkirkwood <scottkirkwood@asta-laptop>
2009-12-10 19:59:01 -0200

    More build automation, this will be very useful for other projects.

--- mkzip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VER='0.12'
-NAME='key-mon'
-rm $NAME-$VER.zip
-cd ..
-zip key-mon/releases/$NAME-$VER.zip key-mon/*.py key-mon/*.kbd key-mon/key-mon key-mon/themes/**/*.svg key-mon/doc/*.rst key-mon/doc/*.txt -x key-mon/setup.py
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 NAME='key-mon'
-VER='0.13'
+VER='0.14'
 setup(
     name = NAME,
     version = VER,
@@ -19,7 +19,7 @@ setup(
     platforms=['POSIX'],
     license='LGPL',
     keywords='keyboard status monitor education',
-    url=['http://code.google.com/p/%s' % NAME],
+    url='http://code.google.com/p/%s' % NAME,
     download_url='http://%s.googlecode.com/files/%ss-%s.zip' % (NAME, NAME, VER),
     classifiers=[
       'Development Status :: 4 - Beta',
@@ -29,6 +29,9 @@ setup(
       'Operating System :: POSIX :: Linux',
       'Topic :: Education :: Computer Aided Instruction (CAI)',
     ], 
-    description='',
-    long_description=""" """,
+    description='A small utility to display your current keyboard and mouse status.  Useful for screencasts.',
+    long_description="""Key-mon is useful for teaching since it shows the current status of your 
+    keyboard and mouse and you use them in another application.  No longer do you need to say
+    'Now I'm pressing the Ctrl-D key', your students can just see the keystroke for themselves.
+    """,
 )
