2cfa95294f9bf2c58a0107de5236166c3ed0f7a9
Scott Kirkwood <scott@forusers.com>
2010-07-16 11:25:39 -0300

    Version 1.2.5 created. Added PT_BR although incomplete. Updated .desktop for other languages.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from distutils.core import setup
 import re
 
 NAME = 'key-mon'
-VER = '1.2.4'
+VER = '1.2.5'
 DIR = 'src/keymon'
 PY_NAME = 'key_mon'
 DEB_NAME = NAME.replace('-', '')
@@ -35,8 +35,9 @@ GOOGLE_CODE_EMAIL = 'scott@forusers.com'
 KEYWORDS = ['keyboard', 'status', 'monitor', 'education']
 MAN_FILE = 'man/%s.1' % NAME
 DESKTOP_FILE = 'icons/%s.desktop' % NAME
-ICON = 'icons/key-mon.xpm'
+ICON = 'icons/%s.xpm' % NAME
 COMMAND = '/usr/bin/%s' % NAME
+LANGS = ['pt_BR']
 
 SETUP = dict(
   name=NAME,
@@ -45,7 +46,9 @@ SETUP = dict(
   package_dir={
       'keymon': 'src/keymon'},
   package_data = {
-      'keymon': ['themes/apple/*', 'themes/classic/*', '*.kbd', 'icons/key-mon.desktop'],
+      'keymon': [
+          'themes/apple/*', 'themes/classic/*', '*.kbd',
+          'icons/key-mon.desktop', 'locale/**/*/*.mo'],
   },
   scripts=['src/key-mon'],
   author=AUTHOR_NAME,
@@ -68,34 +71,7 @@ SETUP = dict(
       'Operating System :: POSIX :: Linux',
       'Topic :: Education :: Computer Aided Instruction (CAI)',
   ],
-  #zip_safe=False,
 )
-
-COPYRIGHT = 'Copyright (C) 2010 %s' % (COPYRIGHT_NAME) # pylint: disable-msg=W0622
-LICENSE_TITLE = 'Apache License'
-LICENSE_SHORT = 'Apache'
-LICENSE_VERSION = '2.0'
-LICENSE_TITLE_AND_VERSION = '%s version %s' % (LICENSE_TITLE, LICENSE_VERSION)
-LICENSE = '%s or any later version' % LICENSE_TITLE_AND_VERSION # pylint: disable-msg=W0622
-LICENSE_TITLE_AND_VERSION_ABBREV = '%s v%s' % (LICENSE_SHORT, LICENSE_VERSION)
-LICENSE_ABBREV = '%s+' % LICENSE_TITLE_AND_VERSION_ABBREV
-LICENSE_URL = 'http://www.apache.org/licenses/LICENSE-2.0'
-LICENSE_PATH = '/usr/share/common-licenses/Apache-2.0'
-LICENSE_NOTICE = '''%(name)s is free software: you can redistribute it and/or modify
-it under the terms of the Apache License as published by
-the Apache Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-%(name)s is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the Apache License
-along with this program.  If not, see <%(url)s>.''' % dict(name=NAME, url=LICENSE_URL)
-LICENSE_NOTICE_HTML = '<p>%s</p>' % LICENSE_NOTICE.replace('\n\n', '</p><p>')
-LICENSE_NOTICE_HTML = re.sub(r'<http([^>]*)>', r'<a href="http\1" target="_blank">http\1</a>',
-    LICENSE_NOTICE_HTML)
 
 if __name__ == '__main__':
   setup(**SETUP)
