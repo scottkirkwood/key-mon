68bbfe38294e4a68951c6caa9da6cfc1d877e912
Scott Kirkwood <scott@forusers.com>
2010-08-02 13:53:43 -0300

    Options are now handled by the option.Options class.

--- src/keymon/key_mon.py
+++ src/keymon/key_mon.py
@@ -36,7 +36,7 @@ except ImportError:
   print 'Error: Missing xlib, run sudo apt-get install python-xlib'
   sys.exit(-1)
 
-import config
+import options
 import lazy_pixbuf_creator
 import mod_mapper
 import settings
@@ -83,7 +83,7 @@ class KeyMon:
     settings.SettingsDialog.register()
     self.options = options
     self.pathname = os.path.dirname(__file__)
-    self.scale = config.get("ui", "scale", float)
+    self.scale = self.options.scale
     if self.scale < 1.0:
       self.svg_size = '-small'
     else:
@@ -102,15 +102,15 @@ class KeyMon:
     self.buttons = None
 
     self.enabled = {
-        'MOUSE': config.get("buttons", "mouse", bool),
-        'SHIFT': config.get("buttons", "shift", bool),
-        'CTRL': config.get("buttons", "ctrl", bool),
-        'META': config.get("buttons", "meta", bool),
-        'ALT': config.get("buttons", "alt", bool),
+        'MOUSE': self.options.mouse,
+        'SHIFT': self.options.shift,
+        'CTRL': self.options.ctrl,
+        'META': self.options.meta,
+        'ALT': self.options.alt,
     }
-    self.emulate_middle = config.get("devices", "emulate_middle", bool)
-    self.modmap = mod_mapper.safely_read_mod_map(config.get("devices", "map"))
-    self.swap_buttons = config.get("devices", "swap_buttons", bool)
+    self.emulate_middle = self.options.emulate_middle
+    self.modmap = mod_mapper.safely_read_mod_map(self.options.kbd_file)
+    self.swap_buttons = self.options.swap_buttons
 
     self.name_fnames = self.create_names_to_fnames()
     self.pixbufs = lazy_pixbuf_creator.LazyPixbufCreator(self.name_fnames,
@@ -233,7 +233,7 @@ class KeyMon:
     self.window.set_title('Keyboard Status Monitor')
     width, height = 30 * self.scale, 48 * self.scale
     self.window.set_default_size(int(width), int(height))
-    self.window.set_decorated(config.get("ui", "decorated", bool))
+    self.window.set_decorated(self.options.decorated)
 
     self.mouse_indicator_win = shaped_window.ShapedWindow(self.svg_name('mouse-indicator'))
 
@@ -302,8 +302,8 @@ class KeyMon:
     self.hbox.show()
     self.add_events()
 
-    old_x = config.get('position', 'x', int)
-    old_y = config.get('position', 'y', int)
+    old_x = self.options.x_pos
+    old_y = self.options.y_pos
     if old_x != -1 and old_y != -1 and old_x and old_y:
       self.window.move(old_x, old_y)
     self.window.show()
@@ -311,11 +311,11 @@ class KeyMon:
   def svg_name(self, fname):
     """Return an svg filename given the theme, system."""
     fullname = os.path.join(self.pathname, 'themes/%s/%s%s.svg' % (
-        config.get("ui", "theme"), fname, self.svg_size))
+        self.options.theme, fname, self.svg_size))
     if self.svg_size and not os.path.exists(fullname):
       # Small not found, defaulting to large size
       fullname = os.path.join(self.pathname, 'themes/%s/%s.svg' %
-                              (config.get("ui", "theme"), fname))
+                              (self.options.theme, fname))
     return fullname
 
   def add_events(self):
@@ -347,8 +347,8 @@ class KeyMon:
     """The window has moved position, save it."""
     x, y = widget.get_position()
     logging.info('Moved window to %d, %d' % (x, y))
-    config.set_config('position', 'x', x)
-    config.set_config('position', 'y', y)
+    self.options.x_pos = x
+    self.options.y_pos = y
 
   def on_idle(self):
     """Check for events on idle."""
@@ -460,7 +460,7 @@ class KeyMon:
     x, y, _ = root.get_pointer()
     w, h = self.mouse_indicator_win.get_size()
     self.mouse_indicator_win.move(x - w/2, y - h/2)
-    if value == 0 and config.get('ui', 'visible-click', bool):
+    if value == 0 and self.options.visible_click:
       self.mouse_indicator_win.fade_away()
     return True
 
@@ -481,7 +481,7 @@ class KeyMon:
   def destroy(self, unused_widget, unused_data=None):
     """Also quit the program."""
     self.devices.stop_listening()
-    config.cleanup()
+    self.options.save()
     gtk.main_quit()
 
   def right_click_handler(self, unused_widget, event):
@@ -520,11 +520,11 @@ class KeyMon:
   def toggle_chrome(self, current):
     """Toggle whether the window has chrome or not."""
     self.window.set_decorated(not current)
-    config.set_config('ui', 'decorated', not current)
+    self.options.decorated = not self.options.decorated
 
   def show_settings_dlg(self, unused_arg):
     """Show the settings dialog."""
-    dlg = settings.SettingsDialog(self.window)
+    dlg = settings.SettingsDialog(self.window, self.options)
     dlg.connect('settings-changed', self.settings_changed)
     dlg.show_all()
     dlg.run()
@@ -533,18 +533,18 @@ class KeyMon:
   def settings_changed(self, unused_dlg):
     """Event received from the settings dialog."""
     self._toggle_a_key(self.mouse_image, 'MOUSE',
-        config.get('buttons', 'mouse', bool))
+        self.options.mouse)
     self._toggle_a_key(self.meta_image, 'META',
-        config.get('buttons', 'meta', bool))
+        self.options.meta)
     self._toggle_a_key(self.shift_image, 'SHIFT',
-        config.get('buttons', 'shift', bool))
+        self.options.shift)
     self._toggle_a_key(self.ctrl_image, 'CTRL',
-        config.get('buttons', 'ctrl', bool))
+        self.options.ctrl)
     self._toggle_a_key(self.alt_image, 'ALT',
-        config.get('buttons', 'ALT', bool))
-    if config.get('ui', 'visible-click', bool):
+        self.options.alt)
+    if self.options.visible_click:
       self.mouse_indicator_win.fade_away()
-    self.window.set_decorated(config.get('ui', 'decorated', bool))
+    self.window.set_decorated(self.options.decorated)
 
   def _toggle_a_key(self, image, name, show):
     """Toggle show/hide a key."""
@@ -564,96 +564,107 @@ def show_version():
   print _('Keymon version %s.') % __version__
   print _('Written by %s') % __author__
 
+def create_options():
+  opts = options.Options()
+
+  opts.add_option(opt_short='-s', opt_long='--smaller', dest='smaller', default=False,
+                  type='bool',
+                  help=_('Make the dialog 25% smaller than normal.'))
+  opts.add_option(opt_short='-l', opt_long='--larger', dest='larger', default=False,
+                  type='bool',
+                  help=_('Make the dialog 25% larger than normal.'))
+  opts.add_option(opt_short='-m', opt_long='--meta', dest='meta', type='bool',
+                  ini_group='buttons', ini_name='meta',
+                  default=False,
+                  help=_('Show the meta (windows) key.'))
+  opts.add_option(opt_long='--mouse', dest='mouse', type='bool', default=True,
+                  ini_group='buttons', ini_name='mouse',
+                  help=_('Show the mouse.'))
+  opts.add_option(opt_long='--shift', dest='shift', type='bool', default=True,
+                  ini_group='buttons', ini_name='shift',
+                  help=_('Show shift key.'))
+  opts.add_option(opt_long='--ctrl', dest='ctrl', type='bool', default=True,
+                  ini_group='buttons', ini_name='ctrl',
+                  help=_('Show the ctrl key.'))
+  opts.add_option(opt_long='--alt', dest='alt', type='bool', default=True,
+                  ini_group='buttons', ini_name='alt',
+                  help=_('Show the alt key.'))
+  opts.add_option(opt_long='--scale', dest='scale', type='float', default=1.0,
+                  ini_group='ui', ini_name='scale',
+                  help=_('Scale the dialog. ex. 2.0 is 2 times larger, 0.5 is '
+                         'half the size. Defaults to %default'))
+  opts.add_option(opt_long='--decorated', dest='decorated', type='bool',
+                  ini_group='ui', ini_name='decorated',
+                  default=False,
+                  help=_('Show decoration'))
+  opts.add_option(opt_long='--visible_click', dest='visible_click', type='bool',
+                  ini_group='ui', ini_name='visible-click',
+                  default=False,
+                  help=_('Show where you clicked'))
+  opts.add_option(opt_long='--kbdfile', dest='kbd_file',
+                  ini_group='devices', ini_name='map',
+                  default='us.kbd',
+                  help=_('Use this kbd filename instead running xmodmap.'))
+  opts.add_option(opt_long='--swap', dest='swap_buttons', type='bool',
+                  default=False,
+                  ini_group='devices', ini_name='swap_buttons',
+                  help=_('Swap the mouse buttons.'))
+  opts.add_option(opt_long='--emulate-middle', dest='emulate_middle', type='bool',
+                  default=False,
+                  ini_group='devices', ini_name='emulate_middle',
+                  help=_('When you press the left, and right mouse buttons at the same time, '
+                         'it displays as a middle mouse button click. '))
+  opts.add_option(opt_short='-v', opt_long='--version', dest='version', type='bool',
+                  help=_('Show version information and exit.'))
+  opts.add_option(opt_short='-t', opt_long='--theme', dest='theme', type='str',
+                  ini_group='ui', ini_name='theme', default='classic',
+                  help=_('The theme to use when drawing status images (ex. "-t apple").'))
+  opts.add_option(opt_long='--list-themes', dest='list_themes', type='bool',
+                  help=_('List available themes'))
+  opts.add_option(opt_long='--old-keys', dest='old_keys', type='int',
+                  ini_group='buttons', ini_name='old-keys',
+                  help=_('How many historical keypresses to show (defaults to %default)'),
+                  default=0)
+
+  opts.add_option(opt_short=None, opt_long=None, type='float',
+                  dest='opacity', default=1.0, help='Opacity of window',
+                  ini_group='ui', ini_name='opacity')
+  opts.add_option(opt_short=None, opt_long=None, type='int',
+                  dest='x_pos', default=-1, help='Last X Position',
+                  ini_group='position', ini_name='x')
+  opts.add_option(opt_short=None, opt_long=None, type='int',
+                  dest='y_pos', default=-1, help='Last Y Position',
+                  ini_group='position', ini_name='y')
+
+  opts.add_option_group(_('Developer Options'), _('These options are for developers.'))
+  opts.add_option(opt_short='-d', opt_long='--debug', dest='debug', type='bool',
+                  default=False,
+                  help=_('Output debugging information.'))
+  opts.add_option(opt_long='--screenshot', dest='screenshot', type='str', default='',
+                  help=_('Create a "screenshot.png" and exit. '
+                         'Pass a comma separated list of keys to simulate'
+                         '(ex. "KEY_A,KEY_LEFTCTRL").'))
+  return opts
+
+
 def main():
   """Run the program."""
-  import optparse
-  parser = optparse.OptionParser()
-  parser.add_option('-s', '--smaller', dest='smaller', default=False, action='store_true',
-                    help=_('Make the dialog 25% smaller than normal.'))
-  parser.add_option('-l', '--larger', dest='larger', default=False, action='store_true',
-                    help=_('Make the dialog 25% larger than normal.'))
-  parser.add_option('-m', '--meta', dest='meta', action='store_true',
-                    default=config.get('buttons', 'meta', bool),
-                    help=_('Show the meta (windows) key.'))
-  parser.add_option('--nometa', dest='meta', action='store_false',
-                    help=_('Don\'t show the meta (windows) key.'))
-  parser.add_option('--mouse', dest='nomouse', action='store_false',
-                    default=not config.get('buttons', 'mouse', bool),
-                    help=_('Show the mouse.'))
-  parser.add_option('--nomouse', dest='nomouse', action='store_true',
-                    help=_('Hide the mouse.'))
-  parser.add_option('--shift', dest='noshift', action='store_false',
-                    default=not config.get('buttons', 'shift', bool),
-                    help=_('Show shift key.'))
-  parser.add_option('--noshift', dest='noshift', action='store_true',
-                    help=_('Don\'t show the shift key.'))
-  parser.add_option('--ctrl', dest='noctrl', action='store_false',
-                    default=not config.get('buttons', 'ctrl', bool),
-                    help=_('Show the ctrl key.'))
-  parser.add_option('--noctrl', dest='noctrl', action='store_true',
-                    help=_('Hide the ctrl key.'))
-  parser.add_option('--alt', dest='noalt', action='store_false',
-                    default=not config.get('buttons', 'alt', bool),
-                    help=_('Show the alt key.'))
-  parser.add_option('--noalt', dest='noalt', action='store_true',
-                    help=_('Hide the alt key.'))
-  parser.add_option('--scale', dest='scale',
-                    default=config.get('ui', 'scale', float),
-                    type='float',
-                    help=_('Scale the dialog. ex. 2.0 is 2 times larger, 0.5 is '
-                           'half the size. Defaults to %default'))
-  parser.add_option('--decorated', dest='decorated', action='store_true',
-                    default=config.get('ui', 'decorated', bool),
-                    help=_('Show decoration'))
-  parser.add_option('--notdecorated', dest='decorated', action='store_false',
-                    help=_('No decoration'))
-  parser.add_option('--visible_click', dest='visible_click', action='store_true',
-                    default=config.get('ui', 'visible-click', bool),
-                    help=_('Show where you clicked'))
-  parser.add_option('--novisible_click', dest='visible_click', action='store_false',
-                    help=_('Turn off the visible button clicks.'))
-  parser.add_option('--kbdfile', dest='kbd_file',
-                    default=config.get('devices', 'map'),
-                    help=_('Use this kbd filename instead running xmodmap.'))
-  parser.add_option('--swap', dest='swap_buttons', action='store_true',
-                    help=_('Swap the mouse buttons.'))
-  parser.add_option('--emulate-middle', dest='emulate_middle', action='store_true',
-                    help=_('When you press the left, and right mouse buttons at the same time, '
-                           'it displays as a middle mouse button click. '))
-  parser.add_option('-v', '--version', dest='version', action='store_true',
-                    help=_('Show version information and exit.'))
-  parser.add_option('-t', '--theme', dest='theme',
-                    default=config.get('ui', 'theme'),
-                    help=_('The theme to use when drawing status images (ex. "-t apple").'))
-  parser.add_option('--list-themes', dest='list_themes', action='store_true',
-                    help=_('List available themes'))
-  parser.add_option('--old-keys', dest='old_keys', type='int',
-                    help=_('How many historical keypresses to show (defaults to %default)'),
-                    default=config.get('buttons', 'old-keys', int))
+  opts = create_options()
+  opts.read_ini_file('~/.config/key-mon/config')
+  opts.parse_args()
 
-  group = optparse.OptionGroup(parser, _('Developer Options'),
-                    _('These options are for developers.'))
-  group.add_option('-d', '--debug', dest='debug', action='store_true',
-                    help=_('Output debugging information.'))
-  group.add_option('--screenshot', dest='screenshot',
-                    help=_('Create a "screenshot.png" and exit. '
-                    'Pass a comma separated list of keys to simulate (ex. "KEY_A,KEY_LEFTCTRL").'))
-  parser.add_option_group(group)
-
-  (options, unused_args) = parser.parse_args()
-
-  if options.version:
+  if opts.version:
     show_version()
     sys.exit(0)
-  if options.debug:
+  if opts.debug:
     logging.basicConfig(
         level=logging.DEBUG,
         format = '%(filename)s [%(lineno)d]: %(levelname)s %(message)s')
-  if options.smaller:
-    options.scale = 0.75
-  elif options.larger:
-    options.scale = 1.25
-  if options.list_themes:
+  if opts.smaller:
+    opts.scale = 0.75
+  elif opts.larger:
+    opts.scale = 1.25
+  if opts.list_themes:
     print _('Available themes:')
     for entry in sorted(os.listdir('themes')):
       try:
@@ -665,26 +676,7 @@ def main():
         pass
     raise SystemExit()
 
-  config.set_config('ui', 'scale', options.scale)
-  config.set_config('ui', 'theme', options.theme)
-
-  if options.nomouse is not None:
-    config.set_config('buttons', 'mouse', not options.nomouse)
-  if options.noshift is not None:
-    config.set_config('buttons', 'shift', not options.noshift)
-  if options.noctrl is not None:
-    config.set_config('buttons', 'ctrl', not options.noctrl)
-  if options.noalt is not None:
-    config.set_config('buttons', 'alt', not options.noalt)
-  if options.meta is not None:
-    config.set_config('buttons', 'meta', options.meta)
-  config.set_config('buttons', 'old-keys', options.old_keys)
-
-  config.set_config('devices', 'map', options.kbd_file)
-  config.set_config('devices', 'emulate_middle', bool(options.emulate_middle))
-  config.set_config('devices', 'swap_buttons', bool(options.swap_buttons))
-
-  keymon = KeyMon(options)
+  keymon = KeyMon(opts)
   try:
     gtk.main()
   except KeyboardInterrupt:
--- src/keymon/settings.py
+++ src/keymon/settings.py
@@ -18,10 +18,9 @@
 
 __author__ = 'scott@forusers.com (Scott Kirkwood)'
 
+import gettext
 import gobject
 import gtk
-import config
-import gettext
 import logging
 
 LOG = logging.getLogger('settings')
@@ -35,10 +34,11 @@ class SettingsDialog(gtk.Dialog):
           gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ())
   }
 
-  def __init__(self, view):
+  def __init__(self, view, options):
     gtk.Dialog.__init__(self, title='Preferences', parent=view,
         flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
         buttons=(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+    self.options = options
     self.set_default_size(350, 350)
     self.connect('response', self._response)
     self.notebook = gtk.Notebook()
@@ -79,19 +79,19 @@ class CommonFrame(gtk.Frame):
     """Do nothing."""
     pass
 
-  def _add_check(self, vbox, title, option, sub_option):
+  def _add_check(self, vbox, title, option):
     """Add a check button."""
     check_button = gtk.CheckButton(label=title)
-    val = config.get(option, sub_option, bool)
-    logging.info('got option %s/%s as %s', option, sub_option, val)
+    val = getattr(self.settings.options, option)
+    logging.info('got option %s as %s', option, val)
     if val:
       check_button.set_active(True)
     else:
       check_button.set_active(False)
-    check_button.connect('toggled', self._toggled, option, sub_option)
+    check_button.connect('toggled', self._toggled, option)
     vbox.pack_start(check_button, False, False)
 
-  def _add_dropdown(self, vbox, title, opt_lst, option, sub_option):
+  def _add_dropdown(self, vbox, title, opt_lst, option):
     """Add a dropdown box."""
     hbox = gtk.HBox()
     label = gtk.Label(title)
@@ -100,33 +100,32 @@ class CommonFrame(gtk.Frame):
     combo = gtk.combo_box_new_text()
     for opt in opt_lst:
       combo.append_text(str(opt))
-    val = config.get(option, sub_option, int)
+    val = getattr(self.settings.options, option)
     combo.set_active(val)
     hbox.pack_start(combo, expand=False, fill=False, padding=10)
-    logging.info('got option %s/%s as %s', option, sub_option, val)
-    combo.connect('changed', self._combo_changed, option, sub_option)
+    logging.info('got option %s as %s', option, val)
+    combo.connect('changed', self._combo_changed, option)
 
     vbox.pack_start(hbox, expand=False, fill=False)
 
-  def _toggled(self, widget, option, sub_option):
+  def _toggled(self, widget, option):
     """The checkbox was toggled."""
     if widget.get_active():
       val = 1
     else:
       val = 0
-    self._update_option(option, sub_option, val)
+    self._update_option(option, val)
 
-  def _combo_changed(self, widget, option, sub_option):
+  def _combo_changed(self, widget, option):
     """The combo box changed."""
     val = widget.get_active()
-    self._update_option(option, sub_option, val)
+    self._update_option(option, val)
 
-  def _update_option(self, option, sub_option, val):
+  def _update_option(self, option, val):
     """Update an option."""
-    config.set_config(option, sub_option, val)
-    config.write()
-    config.cleanup()
-    LOG.info('Set option %s/%s to %s' % (option, sub_option, val))
+    setattr(self.settings.options, option, val)
+    LOG.info('Set option %s to %s' % (option, val))
+    self.settings.options.save()
     self.settings.settings_changed()
 
 class MiscFrame(CommonFrame):
@@ -137,11 +136,11 @@ class MiscFrame(CommonFrame):
   def create_layout(self):
     """Create the box's layout."""
     vbox = gtk.VBox()
-    self._add_check(vbox, _('Swap left-right mouse buttons'), 'devices', 'swap_buttons')
+    self._add_check(vbox, _('Swap left-right mouse buttons'), 'swap_buttons')
     self._add_check(vbox, _('Left+right buttons emulates middle mouse button'),
-       'devices', 'emulate_middle')
-    self._add_check(vbox, _('Highly visible click'), 'ui', 'visible-click')
-    self._add_check(vbox, _('Window decoration'), 'ui', 'decorated')
+       'emulate_middle')
+    self._add_check(vbox, _('Highly visible click'), 'visible_click')
+    self._add_check(vbox, _('Window decoration'), 'decorated')
     self.add(vbox)
 
 class ButtonsFrame(CommonFrame):
@@ -154,28 +153,31 @@ class ButtonsFrame(CommonFrame):
     """Create the layout for buttons."""
     vbox = gtk.VBox()
 
-    self._add_check(vbox, _('_Mouse'), 'buttons', 'mouse')
-    self._add_check(vbox, _('_Shift'), 'buttons', 'shift')
-    self._add_check(vbox, _('_Ctrl'), 'buttons', 'ctrl')
-    self._add_check(vbox, _('Meta (_windows keys)'), 'buttons', 'meta')
-    self._add_check(vbox, _('_Alt'), 'buttons', 'alt')
-    self._add_dropdown(vbox, _('Old Keys:'), [0, 1, 2, 3, 4], 'buttons', 'old-keys')
+    self._add_check(vbox, _('_Mouse'), 'mouse')
+    self._add_check(vbox, _('_Shift'), 'shift')
+    self._add_check(vbox, _('_Ctrl'), 'ctrl')
+    self._add_check(vbox, _('Meta (_windows keys)'), 'meta')
+    self._add_check(vbox, _('_Alt'), 'alt')
+    self._add_dropdown(vbox, _('Old Keys:'), [0, 1, 2, 3, 4], 'old_keys')
     self.add(vbox)
 
 def _test_settings_changed(widget):
   """Help to test if the settings change message is received."""
-  print widget
   print 'Settings changed'
 
 
 def test_dialog():
   """Test the dialog without starting keymon."""
+  import key_mon
+
   SettingsDialog.register()
   gettext.install('key_mon', 'locale')
   logging.basicConfig(
       level=logging.DEBUG,
       format = '%(filename)s [%(lineno)d]: %(levelname)s %(message)s')
-  dlg = SettingsDialog(None)
+  options = key_mon.create_options()
+  options.read_ini_file('~/.config/key-mon/config')
+  dlg = SettingsDialog(None, options)
   dlg.connect('settings-changed', _test_settings_changed)
   dlg.show_all()
   dlg.run()
