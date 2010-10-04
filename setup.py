b6ed255c426b5a8dfedc4623860c5e8fa8339a91
Scott Kirkwood <scott@forusers.com>
2010-10-04 20:08:21 -0300

    Version 1.4.3.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import gettext
 NAME = 'key-mon'
 DIR = 'src/keymon'
 gettext.install(NAME, DIR + '/locale')
-VER = '1.4.2'
+VER = '1.4.3'
 PY_NAME = 'key_mon'
 DEB_NAME = NAME.replace('-', '')
 RELEASE_FILE = 'RELEASE.rst'
