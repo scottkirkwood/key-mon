e90dcd9e6df5bb52fb36b04dd77cd3259af9ede3
Scott Kirkwood <scott@forusers.com>
2010-10-31 10:37:04 -0200

    Added patch from stefantapalaru (thanks!).

---
+++ src/keymon/shaped_window.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# Copyright 2010 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Create a shaped window to show mouse events.
+
+Thanks to mathias.gumz for the original code.
+"""
+import gobject
+import gtk
+
+import lazy_pixbuf_creator
+
+class ShapedWindow(gtk.Window):
+  """Create a window shaped as fname."""
+  def __init__(self, fname, scale=1.0):
+    gtk.Window.__init__(self)
+    self.connect('size-allocate', self._on_size_allocate)
+    self.set_decorated(False)
+    self.set_keep_above(True)
+    self.scale = scale
+    self.name_fnames = {
+      'mouse' : [fname],
+    }
+    self.pixbufs = lazy_pixbuf_creator.LazyPixbufCreator(self.name_fnames,
+                                                         self.scale)
+    self.pixbuf = self.pixbufs.get('mouse')
+    self.resize(self.pixbuf.get_width(), self.pixbuf.get_height())
+
+    # a pixmap widget to contain the pixmap
+    self.image = gtk.Image()
+    bitmap, self.mask = self.pixbuf.render_pixmap_and_mask()
+    self.image.set_from_pixmap(bitmap, self.mask)
+    self.image.show()
+    self.add(self.image)
+
+  def _on_size_allocate(self, win, unused_allocation):
+    """Called when first allocated."""
+    # Set the window shape
+    win.shape_combine_mask(self.mask, 0, 0)
+    win.set_property('skip-taskbar-hint', True)
+    if not win.is_composited():
+      print 'Unable to fade the window'
+    else:
+      win.set_opacity(0.5)
+
+  def fade_away(self):
+    """Make the window fade in a little bit."""
+    self.present()
+    gobject.timeout_add(200, self.hide)
