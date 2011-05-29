ebd464f3566f42ecca5182fe78aff4ff6111ab06
Scott Kirkwood <scott@forusers.com>
2011-05-29 19:38:58 -0400

    Finicky RELEASE notes.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import gettext
 NAME = 'key-mon'
 DIR = 'src/keymon'
 gettext.install(NAME, DIR + '/locale')
-VER = '1.5.1'
+VER = '1.5.2'
 PY_NAME = 'key_mon'
 DEB_NAME = NAME.replace('-', '')
 RELEASE_FILE = 'RELEASE.rst'
