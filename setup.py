377b1d67f189ce101580b2a698c125529f378265
Scott Kirkwood <scott@forusers.com>
2012-06-27 10:02:14 -0400

    Version 1.13.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import gettext
 NAME = 'key-mon'
 DIR = 'src/keymon'
 gettext.install(NAME, DIR + '/locale')
-VER = '1.12'
+VER = '1.13'
 PY_NAME = 'key_mon'
 DEB_NAME = NAME.replace('-', '')
 RELEASE_FILE = 'RELEASE.rst'
--- src/keymon/key_mon.py
+++ src/keymon/key_mon.py
@@ -20,7 +20,7 @@ Shows their status graphically.
 """
 
 __author__ = 'Scott Kirkwood (scott+keymon@forusers.com)'
-__version__ = '1.12'
+__version__ = '1.13'
 
 import locale
 import logging
