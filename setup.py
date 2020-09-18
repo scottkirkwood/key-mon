33765d084a18d4d5c6dc3e2ae3ab65f1354d8551
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2020-09-18 10:49:37 +0100

    Fix install script
    Was causing an error on install:
    > PY_SRC = f'{PY_NAME}.py' % PY_NAME
    > TypeError: not all arguments converted during string formatting

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ DEB_NAME = NAME.replace('-', '')
 RELEASE_FILE = 'RELEASE.rst'
 VCS = 'https://github.com/scottkirkwood/key-mon.git'
 
-PY_SRC = f'{PY_NAME}.py' % PY_NAME
+PY_SRC = f'{PY_NAME}.py'
 DEPENDS = ['python3-xlib', 'python3-gi-cairo', 'gir1.2-gtk-3.0']
 DEPENDS_STR = ' '.join(DEPENDS)
 
